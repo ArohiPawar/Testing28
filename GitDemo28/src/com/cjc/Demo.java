193f386d0c7e7377c7f8d8484b704dc901777d0c
amitcjc <amitcjcpune@gmail.com>
2024-12-09 20:04:05 +0530

    by amit

--- GitDemo28/src/com/cjc/Demo.java
+++ GitDemo28/src/com/cjc/Demo.java
@@ -2,7 +2,17 @@ package com.cjc;
 
 public class Demo 
 {
+	
+	public void m1()
+	{
+	System.out.println("M1...");	
+	}
+	
+	
 public static void main(String[] args) {
 	System.out.println("This is Demo Main");
 }
+
+
+
 }
