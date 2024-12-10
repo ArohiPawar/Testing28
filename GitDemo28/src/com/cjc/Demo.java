ad0a2d0376fb08f78625a13a972423fb333334ad
amitcjc <amitcjcpune@gmail.com>
2024-12-10 19:25:35 +0530

    by abc

--- GitDemo28/src/com/cjc/Demo.java
+++ GitDemo28/src/com/cjc/Demo.java
@@ -8,6 +8,12 @@ public class Demo
 	System.out.println("M1...");	
 	}
 	
+	public void m2()
+	{
+		System.out.println("Hello M2");
+	}
+	
+	
 	
 public static void main(String[] args) {
 	System.out.println("This is Demo Main");
